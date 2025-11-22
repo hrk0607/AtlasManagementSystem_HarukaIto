2166b77c518fc9dbb715cc401b3bb697dac5d802
haruka ito <>
2025-11-23 07:57:08 +0900

    ■キャンセル確認のモーダルにあるキャンセルボタンを押下すると予約をキャンセル機能を追加する。 ■閉じるボタンを押下するとモーダルを閉じる

--- resources/views/authenticated/calendar/general/calendar.blade.php
+++ resources/views/authenticated/calendar/general/calendar.blade.php
@@ -42,5 +42,7 @@
   </div>
 </x-sidebar>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 <script src="{{ asset('js/calendar.js') }}"></script>
