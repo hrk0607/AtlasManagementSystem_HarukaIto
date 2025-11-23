a6e92f5ebd2c45f8774b8f4df8c6ea94bc700288
haruka ito <>
2025-11-24 08:40:12 +0900

    ■対象の部数に予約したユーザーの下記３つの情報を表示する ・ユーザーのID ・ユーザーの名前 ・予約場所（リモートのみ）

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -1,17 +1,17 @@
 <x-sidebar>
   <div class="w-75 m-auto h-75">
-    <div class="calendar-wrapper">
+    <div class="reserve-wrapper">
       <p>
-        <span>{{ $date }}</span>
+        <span>{{ \Carbon\Carbon::parse($date)->format('Y年n月j日') }}</span>
         <span class="ml-3">{{ $part }}部</span>
       </p>
 
-      <div class="calendar-inner">
-        <table class="">
-          <tr class="text-center">
-            <th class="w-25">ID</th>
-            <th class="w-25">名前</th>
-            <th class="w-25">場所</th>
+      <div class="reserve-inner">
+        <table class="reserve-table">
+          <tr class="table-header">
+            <th class="w-1/5">ID</th>
+            <th class="[width:40%]">名前</th>
+            <th class="[width:40%]">場所</th>
           </tr>
 
           @php
@@ -20,15 +20,15 @@
 
           @if($setting && $setting->users->count())
           @foreach ($setting->users as $user)
-          <tr class="text-center">
-            <td class="w-25">{{ $user->id }}</td>
-            <td class="w-25">{{ $user->name }}</td>
-            <td class="w-25">{{ $user->place ?? '-' }}</td>
+          <tr class="table-row">
+            <td class="w-1/5">{{ $user->id }}</td>
+            <td class="[width:40%]">{{ $user->over_name }} {{ $user->under_name }}</td>
+            <td class="[width:40%]">リモート</td>
           </tr>
 
           @endforeach
           @else
-          <tr class="text-center">
+          <tr class="table-row">
             <td colspan="3">予約者はいません</td>
           </tr>
           @endif
